658bc7bb2af5621188d2e6f9b990336280d61c8e
ibrahim <iharby2002@gmail.com>
2026-02-27 22:13:59 +0800

    feat: add sumTotalHours query to AttendanceRepository
    - create query method to fetch employees total hours with specified period

--- src/main/java/com/iodsky/sweldox/attendance/AttendanceRepository.java
+++ src/main/java/com/iodsky/sweldox/attendance/AttendanceRepository.java
@@ -3,8 +3,10 @@ package com.iodsky.sweldox.attendance;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -22,5 +24,13 @@ public interface AttendanceRepository extends JpaRepository<Attendance, UUID> {
     Page<Attendance> findByEmployee_IdAndDateBetween(Long employeeId, LocalDate startDate, LocalDate endDate, Pageable pageable);
 
     List<Attendance> findByEmployee_IdAndDateBetween(Long employeeId, LocalDate startDate, LocalDate endDate);
+
+    @Query("""
+    SELECT COALESCE(SUM(a.totalHours), 0)
+    FROM Attendance a
+    WHERE a.employee.id = :employeeId
+    AND a.date BETWEEN :startDate AND :endDate
+    """)
+    BigDecimal sumTotalHoursByEmployee_IdAndDateBetween(Long employeeId, LocalDate startDate, LocalDate endDate);
 
 }
