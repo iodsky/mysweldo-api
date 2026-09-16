af69b5fcea868ad37cf8ac2aaeff140f0e0f4e4a
iodsky <iharby2002@gmail.com>
2026-09-16 21:37:38 +0800

    feat: add AWS credentials support for S3 configuration

--- src/main/java/com/iodsky/mysweldo/common/S3Config.java
+++ src/main/java/com/iodsky/mysweldo/common/S3Config.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -11,9 +13,22 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Profile("prod")
 public class S3Config {
 
+    @Value("${aws.access_key}")
+    private String accessKey;
+
+    @Value("${aws.secret_access_key}")
+    private String secretAccessKey;
+
     @Bean
     public S3Client s3Client(@Value("${aws.region}") String region) {
-        return S3Client.builder().region(Region.of(region)).build();
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretAccessKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(credentials)
+                )
+                .build();
     }
 
 }
